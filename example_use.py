c6c5054466ceeb7d2d75db4949e3a0ff707c5b23
oshritshtossel <oshritvig@gmail.com>
2023-11-01 16:14:08 +0200

    -

--- example_use.py
+++ example_use.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import MIPMLP
 import samba
-from src import apply_mimic
+
+try:
+    from miMic_test import apply_mimic
+except:
+    from src import apply_mimic
 
 if __name__ == '__main__':
 
     # Load the raw data in the required format
     df = pd.read_csv("between/ibd_for_process.csv")
-    tag = pd.read_csv("between/ibd_tag.csv",index_col=0)
-
+    tag = pd.read_csv("between/ibd_tag.csv", index_col=0)
 
     # Apply the MIPMLP with the defaultive parameters
     processed = MIPMLP.preprocess(df)
@@ -18,10 +21,8 @@ if __name__ == '__main__':
     samba.micro2matrix(processed, folder, save=True)
 
     # Apply miMic test
-    taxonomy_selected = apply_mimic(folder, tag,eval="man")
+    taxonomy_selected = apply_mimic(folder, tag, eval="man")
     if taxonomy_selected is not None:
-        apply_mimic(folder, tag,mode="plot",tax=taxonomy_selected,eval="man")
-
-
+        apply_mimic(folder, tag, mode="plot", tax=taxonomy_selected, eval="man")
 
     c = 0
--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
 from .tax_tree_create import create_tax_tree
 from .miMic_test import apply_mimic
+
+__all__ = ["apply_mimic"]
