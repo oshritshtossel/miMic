ea210e700df8ef58daa1be2aff0fce1aff02f2d8
shanif3 <shaninn123@gmail.com>
2024-02-25 11:39:06 +0200

    update example_use.py

--- example_use.py
+++ example_use.py
@@ -2,26 +2,27 @@ import pandas as pd
 import MIPMLP
 import samba
 
-# try:
-#     from mimic import apply_mimic
-# except:
-from src.mimic import apply_mimic
-
+try:
+    from mimic import apply_mimic
+except:
+    from src.mimic import apply_mimic
 
 if __name__ == '__main__':
-    # Load the raw data in the required format
-    df = pd.read_csv("between/ibd_for_process.csv")
-    tag = pd.read_csv("between/ibd_tag.csv", index_col=0)
+    # Load the raw data and tag in the csv format => apply 'preprocess' mode => apply mimic with 'test' mode => apply mimic with 'plot' mode
+    df = pd.read_csv("example_data/for_process.csv")
+    tag = pd.read_csv("example_data/tag.csv", index_col=0)
+    folder = "example_data/2D_images"
 
-    # Apply the MIPMLP with the defaultive parameters
-    processed = MIPMLP.preprocess(df, taxnomy_group="sub PCA")
+    # Apply MIPMLP and samba automatically
+    apply_mimic(folder=folder, tag=tag, mode='preprocess', rowData=df, taxnomy_group='sub PCA')
 
-    # micro2matrix and saving the images in a prepared folder
-    folder = "between/2D_ibd"
-    samba.micro2matrix(processed, folder, save=True)
+    # If you want to apply the MIPMLP and Samba by yourself instead of apply "preprocess" mode as above, you can use the following code:
+    # processed = MIPMLP.preprocess(df, taxnomy_group="sub PCA")
+    # # micro2matrix and saving the images in a prepared folder
+    # samba.micro2matrix(processed, folder, save=True)
 
     # Apply miMic test
-    taxonomy_selected = apply_mimic(folder, tag, eval="man", threshold_p=0.05, save=True)
+    taxonomy_selected = apply_mimic(folder, tag, eval="corr", threshold_p=0.05, save=True)
     if taxonomy_selected is not None:
-        apply_mimic(folder, tag, mode="plot", tax=taxonomy_selected, eval="man", sis='fdr_bh', save=False,
+        apply_mimic(folder, tag, mode="plot", tax=taxonomy_selected, eval="corr", sis='fdr_bh', save=False,
                     threshold_p=0.05, THRESHOLD_edge=0.5)
